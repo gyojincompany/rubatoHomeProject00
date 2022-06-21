359451114b4089de9325da449546e9f81386bb8d
gyojincompany <luckyred@gmail.com>
2022-06-21 13:06:11 +0900

    rubato웹 html 디자인 스프링프레임워크 적용 예제

---
+++ src/main/java/com/gyojincompany/rubatohome/RubatoHomeProject00Application.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.rubatohome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RubatoHomeProject00Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RubatoHomeProject00Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gyojincompany/rubatohome/RubatoHomeProject00ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gyojincompany.rubatohome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RubatoHomeProject00ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
