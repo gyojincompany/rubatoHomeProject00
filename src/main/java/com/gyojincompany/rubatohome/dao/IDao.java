59aa8112bc6dcf0bbc3cc510ded3d6795cb2524b
gyojincompany <luckyred@gmail.com>
2022-06-24 09:40:17 +0900

    게시판 글보기 기능 추가

--- src/main/java/com/gyojincompany/rubatohome/dao/IDao.java
+++ src/main/java/com/gyojincompany/rubatohome/dao/IDao.java
@@ -11,6 +11,8 @@ public interface IDao {
 	public ArrayList<FBoardDto> fblistDao();//자유게시판 리스트 가져오기
 	public int fblistcountDao();//자유게시판 리스트 글 개수 가져오기
 	public void fbhitDao(String fbnum);//자유게시판 조회수 증가
+	public FBoardDto fbviewDao(String fbnum);//자유게시판 글 보기
+	
 	
 	//member 용 dao
 	
